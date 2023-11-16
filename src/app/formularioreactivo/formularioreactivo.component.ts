7d6f4e013556a13e109f6226090f300381e66919
perezguedesmaikel <perezguedesmaikel@gmail.com>
2023-11-16 00:24:33 -0500

    form ready

--- src/app/formularioreactivo/formularioreactivo.component.ts
+++ src/app/formularioreactivo/formularioreactivo.component.ts
@@ -11,13 +11,8 @@ export class FormularioreactivoComponent {
   email: FormControl<string | null> = new FormControl('', [Validators.required, Validators.email])
   formUser = new FormGroup([this.email, this.name])
 
-
-  ngOnInit() {
-    console.log('Estado inicial');
-    this.email.valueChanges.subscribe(() => {
-      console.log('Valor de la variable reactiva:', this.email.errors?.['email'] ?? '');
-      // Realiza acciones cada vez que la variable reactiva cambie de valor
-    });
+  handleSubmit() {
+    console.log({name: this.name.value, email: this.email.value})
   }
 
 }
