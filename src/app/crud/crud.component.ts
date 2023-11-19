bbd82ca0f182f15df2d86270b510dcf7e4296d1a
perezguedesmaikel <perezguedesmaikel@gmail.com>
2023-11-18 23:38:55 -0500

    better crud

--- src/app/crud/crud.component.ts
+++ src/app/crud/crud.component.ts
@@ -1,40 +1,43 @@
 import {Component} from '@angular/core';
 
 interface Item {
-  nombre: string;
-  descripcion: string;
+    id?: number;
+    precio: number;
+    nombre: string;
+    descripcion: string;
 }
 
 @Component({
-  selector: 'app-crud',
-  templateUrl: './crud.component.html',
-  styleUrls: ['./crud.component.css']
+    selector: 'app-crud',
+    templateUrl: './crud.component.html',
+    styleUrls: ['./crud.component.css']
 })
 
 
 export class CrudComponent {
-  items: Item[] = [
-    {nombre: 'Item 1', descripcion: 'Descripción del Item 1'},
-    {nombre: 'Item 2', descripcion: 'Descripción del Item 2'},
-    {nombre: 'Item 3', descripcion: 'Descripción del Item 3'}
-  ];
+    items: Item[] = [
+        {id: 1, nombre: 'Item 1', precio: 10.20, descripcion: 'Descripción del Item 1'},
+        {id: 2, nombre: 'Item 2', precio: 11.20, descripcion: 'Descripción del Item 2'},
+        {id: 3, nombre: 'Item 3', precio: 10.25, descripcion: 'Descripción del Item 3'}
+    ];
 
-  agregar() {
-    const newItem: Item = {
-      nombre: 'Nuevo Item',
-      descripcion: 'Descripción del Nuevo Item'
-    };
-    this.items.push(newItem);
-  }
-
-  editar(item: Item) {
-    // Lógica para editar el item
-  }
-
-  eliminar(item: Item) {
-    const index = this.items.indexOf(item);
-    if (index !== -1) {
-      this.items.splice(index, 1);
+    agregar() {
+        const newItem: Item = {
+            precio: 15,
+            nombre: 'Nuevo Item',
+            descripcion: 'Descripción del Nuevo Item'
+        };
+        this.items.push(newItem);
     }
-  }
+
+    editar(item: Item) {
+        // Lógica para editar el item
+    }
+
+    eliminar(item: Item) {
+        const index = this.items.indexOf(item);
+        if (index !== -1) {
+            this.items.splice(index, 1);
+        }
+    }
 }
